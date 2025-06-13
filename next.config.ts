8b202ff101c832dc36da66ba3ead88970881bf8e
ravi0-code <ravirajk802@gmail.com>
2025-06-13 11:47:39 +0530

    error

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,14 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'upload.wikimedia.org',
+        pathname: '/**',
       },
     ],
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
